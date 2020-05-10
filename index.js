be3c78d512376bb8d0769c020cf75a3be6d61258
Quentin Gaultier <quentin.gaultier@gmail.com>
2020-05-10 12:32:05 +0200

    prefix support

--- index.js
+++ index.js
@@ -9,20 +9,36 @@ let colorize = color => a => {
 };
 
 
+const defaultParams = {
+    prefix: null,
+    levels: {
+        log: {color: 'white'},
+        debug: {color: 'white'},
+        info: {color: 'green'},
+        warn: {color: 'yellow'},
+        error: {color: 'red'},
+    }
+}
 
-const configureNode = () => {
-let params = [['log', 'white'], ['debug', 'white'], ['info', 'green'], ['warn', 'yellow'], ['error', 'red']];
+const configureNode = (params) => {
 
-params.forEach(([level, color]) => {
-    const log = console[level]
-    console[level] = function () {
-        let now = new Date()
-        let formatted_date = `${now.getUTCDate()}-${now.getUTCMonth() + 1}-${now.getUTCFullYear()} ${now.getUTCHours()}:${now.getUTCMinutes()}:${now.getUTCSeconds()}.${now.getUTCMilliseconds()}`
-        const datePrefix = _.padEnd(`[${ formatted_date}]`, 23);
-        const levelPrefix = _.padEnd(`[${ level}]`, 7)
-        log(...([`${datePrefix}`, `${levelPrefix}`, ...arguments].map(colorize(color))))
-    };
-});
+    params = _.merge(defaultParams, params);
+
+    _.entries(params.levels).forEach(([level, {color, prefix}]) => {
+        const log = console[level]
+        console[level] = function () {
+            let now = new Date()
+            let formatted_date = `${now.getUTCDate()}-${now.getUTCMonth() + 1}-${now.getUTCFullYear()} ${now.getUTCHours()}:${now.getUTCMinutes()}:${now.getUTCSeconds()}.${now.getUTCMilliseconds()}`
+            const datePrefix = _.padEnd(`[${ formatted_date}]`, 23);
+            const levelPrefix = _.padEnd(`[${ level}]`, 7)
+            const prefix = params.prefix || prefix
+            const all = [datePrefix, levelPrefix]
+            if (prefix) all.push(prefix)
+            all.push(...arguments)
+
+            log(...all.map(colorize(color)))
+        };
+    });
 
 
 }
